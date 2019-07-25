9da65eeb6e5fd1e61533da4a4fe0f2d91ecdcc94
Amujo Dotun <samue;amujo@gmail.com>
2019-07-25 17:52:08 +0100

    updated the home page to fetch data from database

--- myscrumy/samuelamujoscrumy/views.py
+++ myscrumy/samuelamujoscrumy/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ScrumyGoals,GoalStatus,ScrumyHistory
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 import random
 
@@ -44,18 +45,45 @@ def add_goal(request):
     scrumyGoal.save()
     return HttpResponse('OK')
 
+
 def home(request):
-    User =get_user_model()
-    goal_stats = GoalStatus.objects.get(status_name="Weekly Goal")
+    status = GoalStatus.objects.all()
+    users = User.objects.all()
+    a = GoalStatus.objects.get(status_name="Weekly Goal")
+    goal_stats = a.scrumygoals_set.all()
+    b = GoalStatus.objects.get(status_name="Daily Goal")
+    goal_stats1 = b.scrumygoals_set.all()
+    c = GoalStatus.objects.get(status_name="Verify Goal")
+    goal_stats2 = c.scrumygoals_set.all()
+    d = GoalStatus.objects.get(status_name="Done Goal")
+    goal_stats3 = d.scrumygoals_set.all()
+   
+
+    context = {
+        'status': status,
+        'users': users,
+        'goal_stats': goal_stats,
+        'goal_stats1': goal_stats1,
+        'goal_stats2': goal_stats2,
+        'goal_stats3': goal_stats3,
+    }
+    return render(request, 'samuelamujoscrumy/home.html', context)
+
+
+
+
+# def home(request):
+#     User =get_user_model()
+#     goal_stats = GoalStatus.objects.get(status_name="Weekly Goal")
     
-    user = User.objects.get(id=1)
+#     user = User.objects.get(id=1)
 
-    scrumy_goal = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
+#     scrumy_goal = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
 
     
-    user = ScrumyGoals.objects.all()
+#     user = ScrumyGoals.objects.all()
 
-    return render(request, 'samuelamujoscrumy/home.html', {'user': user, 'scrumy_goal': scrumy_goal})
+#     return render(request, 'samuelamujoscrumy/home.html', {'user': user, 'scrumy_goal': scrumy_goal})
 
 
 # def exception(request,goal_id):
